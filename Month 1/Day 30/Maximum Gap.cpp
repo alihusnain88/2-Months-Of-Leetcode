283581b5aee4047b0bd1ff2c3137d74ddba24521
Ali Husnain <muhammadalihusnain100@gmail.com>
2024-11-06 18:58:29 +0500

    Added Day 30 files

---
+++ Month 1/Day 30/Maximum Gap.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maximumGap(vector<int> &nums)
+    {
+        // With Brute Force
+        int size = nums.size();
+        if (size == 1)
+        {
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int maxdiff = INT_MIN;
+        for (int i = 1; i < size; i++)
+        {
+            maxdiff = max(maxdiff, abs(nums[i] - nums[i - 1]));
+        }
+        return maxdiff;
+    }
+};
